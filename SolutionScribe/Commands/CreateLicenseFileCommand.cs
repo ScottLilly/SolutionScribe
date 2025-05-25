7a8f96d7813331d25f05e81a81eb0ed4141ee644
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2025-05-25 17:09:32 -0400

    Started creating license file

--- SolutionScribe/Commands/CreateLicenseFileCommand.cs
+++ SolutionScribe/Commands/CreateLicenseFileCommand.cs
@@ -1,10 +1,25 @@
-﻿namespace SolutionScribe;
+﻿using SolutionScribe.Windows;
+using System.Windows.Forms;
+
+namespace SolutionScribe;
 
 [Command(PackageIds.CreateLicenseFileCommand)]
 internal sealed class CreateLicenseFileCommand : BaseCommand<CreateLicenseFileCommand>
 {
     protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
     {
-        await VS.MessageBox.ShowAsync("Solution Scribe", "Create LICENSE file executed.");
+        var details = new LicenseDataWindow();
+
+        if (details.ShowDialog() != DialogResult.OK)
+        {
+            return;
+        }
+
+        await CreateLicenseFileAsync(details.PopulatedLicenseText);
+    }
+
+    private async Task CreateLicenseFileAsync(string licenseText)
+    {
+        ;
     }
 }
---
+++ SolutionScribe/Windows/LicenseDataWindow.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SolutionScribe.Windows;
+
+partial class LicenseDataWindow
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboLicenseTypes = new System.Windows.Forms.ComboBox();
+            this.tbYears = new System.Windows.Forms.TextBox();
+            this.tbCopyrightHolder = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "License Type:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Copyright Year(s):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Copyright Holder:";
+            // 
+            // cboLicenseTypes
+            // 
+            this.cboLicenseTypes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboLicenseTypes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboLicenseTypes.FormattingEnabled = true;
+            this.cboLicenseTypes.Location = new System.Drawing.Point(147, 9);
+            this.cboLicenseTypes.Name = "cboLicenseTypes";
+            this.cboLicenseTypes.Size = new System.Drawing.Size(391, 26);
+            this.cboLicenseTypes.TabIndex = 3;
+            // 
+            // tbYears
+            // 
+            this.tbYears.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbYears.Location = new System.Drawing.Point(147, 43);
+            this.tbYears.Name = "tbYears";
+            this.tbYears.Size = new System.Drawing.Size(150, 24);
+            this.tbYears.TabIndex = 4;
+            // 
+            // tbCopyrightHolder
+            // 
+            this.tbCopyrightHolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCopyrightHolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCopyrightHolder.Location = new System.Drawing.Point(147, 77);
+            this.tbCopyrightHolder.Name = "tbCopyrightHolder";
+            this.tbCopyrightHolder.Size = new System.Drawing.Size(391, 24);
+            this.tbCopyrightHolder.TabIndex = 5;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(428, 211);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(109, 27);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "Create File";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // LicenseDataWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 250);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.tbCopyrightHolder);
+            this.Controls.Add(this.tbYears);
+            this.Controls.Add(this.cboLicenseTypes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LicenseDataWindow";
+            this.Text = "License Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.ComboBox cboLicenseTypes;
+    private System.Windows.Forms.TextBox tbYears;
+    private System.Windows.Forms.TextBox tbCopyrightHolder;
+    private System.Windows.Forms.Button btnOK;
+}
